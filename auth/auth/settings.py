87db036020386adc542dff0526066c9102c8dcfc
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2018-10-29 18:16:04 +0000

    create auth_server app

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'auth_server.apps.AuthServerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
