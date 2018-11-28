e97b1945ecc965f33bd745aa82f7e911b8086b81
marta <marta.cruz@tecnico.ulisboa.pt>
2018-11-28 11:24:42 +0000

    Templates and beginning of register view

--- auth/auth_server/views.py
+++ auth/auth_server/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    return render(request, 'auth_server/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth_server/register.html', {'form': form})
+
+def login(request):
+    return render(request, 'auth_server/login.html')
