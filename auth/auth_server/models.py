390a765d8dce1052efdc3d3e32f8c1d48e60fe74
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2018-12-11 18:04:45 +0000

    change totp model key lenght

--- auth/auth_server/models.py
+++ auth/auth_server/models.py
@@ -7,7 +7,7 @@ class UserTwoFactor(models.Model):
     user = AutoOneToOneField(User, primary_key=True, on_delete=models.CASCADE, related_name='two_factor')
 
     two_factor_enabled = models.BooleanField(default=False, null=False)
-    totp_key = models.CharField(max_length=70, default="", null=False)
+    totp_key = models.CharField(max_length=16, default="", null=False)
 
     def __str__(self):
         return self.user.__str__()
