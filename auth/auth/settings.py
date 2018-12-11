f112cf13f1f8eecbcf36bb3186e3b8dab0ffd609
Nandinski <nando_14sclm@hotmail.com>
2018-12-11 18:02:13 +0000

    UI changes

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'captcha',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 try:
     from .local_settings import *
